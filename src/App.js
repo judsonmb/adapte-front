c62e5ff1109d1777e34645870faa11fb344e1ff9
Judson Bandeira <judson.bandeira@fichacerta.com.br>
2021-03-18 16:33:43 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Navbar from './components/navbar'
+import Rotas from './rotas'
+
+
+function App() {
+  return (
+      <>
+        <div className="container">
+          <Navbar />
+          <Rotas />
+        </div>
+      </>
+  );
+}
+
+export default App;
---
+++ src/rotas.js
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import Home from './views/home'
+import CadastroProduto from './views/produtos/cadastro'
+
+export default () => {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/cadastro-produtos" component={CadastroProduto} />
+                <Route exact path="/" component={Home} />
+            </Switch>
+        </Router>
+    )
+}
