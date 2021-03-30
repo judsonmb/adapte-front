74d6755c8fab49c5207a1a1645430b76c8a545f3
Judson Bandeira <judson.bandeira@fichacerta.com.br>
2021-03-30 17:43:07 -0300

    tratando erro na listagem de usuarios

--- src/views/users/index.js
+++ src/views/users/index.js
@@ -8,7 +8,7 @@ class Index extends React.Component{
         apiResponse : undefined
     }
 
-    componentDidMount() {
+    componentDidMount(){
 
         let config = {
             headers: { Authorization: 'Bearer ' + localStorage.getItem('USER_TOKEN') }
@@ -23,17 +23,14 @@ class Index extends React.Component{
         })
         .catch(err => {
             if(err.response){
-                if(err.response.status === 500){
-                    this.setState({ errorMessage : err.data.data.message })
-                }
+                this.setState({ apiResponse : err.response.data })
             }
         })
-         
     }
 
     render(){
+
         if(this.state.apiResponse === undefined){
-            
             return(
                 <div>
                     <div className="card">
@@ -46,45 +43,56 @@ class Index extends React.Component{
                     </div>
                 </div>
             )
-            
         }else{
             return(
-                <div className="card"> 
-                    <div className="card-header">
-                        Home / Usuários
+                <div>
+                    {   
+                        (this.state.apiResponse.message) &&
+                            <div class="alert alert-dismissible alert-danger">
+                                <button type="button" class="close" data-dismiss="alert">&times;</button>
+                                    <strong>{this.state.apiResponse.message}</strong>
+                            </div>
+                        }
+                    <div className="card">
+                        <div className="card-header">
+                            Usuários
+                        </div>   
                     </div>
                     <div className="card-body">
-                        <a href="/usuarios/cadastrar/"><button type="button" className="btn btn-success">Cadastrar</button></a>
-                        <table className="table table-hover">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Nome</th>
-                                    <th scope="col">Email</th>
-                                    <th scope="col">Ações</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {    
-                                   this.state.apiResponse.data.map((user) => {
-                                    return (
-                                            <tr>
-                                                <td>{user.name}</td>
-                                                <td>{user.email}</td>
-                                                <td>
-                                                    <button type="button" className="btn btn-warning">Editar</button>
-                                                    <button type="button" className="btn btn-danger">Excluir</button>
-                                                </td>
-                                            </tr>
-                                        )
-                                   }) 
-                                }
-                            </tbody>
-                        </table>
+                        <div>
+                            <a href="/usuarios/cadastrar/"><button type="button" className="btn btn-success">Cadastrar</button></a>
+                            <table className="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">Nome</th>
+                                        <th scope="col">Email</th>
+                                        <th scope="col">Ações</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {   
+                                        (this.state.apiResponse.data &&
+                                            this.state.apiResponse.data.map((user) => {
+                                                return (
+                                                        <tr>
+                                                            <td>{user.name}</td>
+                                                            <td>{user.email}</td>
+                                                            <td>
+                                                                <button type="button" className="btn btn-warning">Editar</button>
+                                                                <button type="button" className="btn btn-danger">Excluir</button>
+                                                            </td>
+                                                        </tr>
+                                                    )
+                                            })
+                                        ) 
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
-                
             )
-        } 
+        }
     }
 }
 
